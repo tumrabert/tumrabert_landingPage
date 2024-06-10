ce98e52a9fbef5122f6b3c69d906fc501bdca2b1
tumrabert <tum_tanakid@hotmail.com>
2024-06-11 01:21:10 +0700

    make spinner props optional

--- app/components/Spinner/Spinner.tsx
+++ app/components/Spinner/Spinner.tsx
@@ -1,15 +1,15 @@
 // components/Spinner.tsx
-"use client"; // Ensures this is a client-side component
+"use client";  // Ensures this is a client-side component
 
 import { ClipLoader } from "react-spinners";
 
 // Define the Props interface for type-checking
 type SpinnerProps = {
-  isLoading: boolean;
+  isLoading?: boolean; // Make isLoading optional
 };
 
 // Functional component to show the spinner
-const Spinner: React.FC<SpinnerProps> = ({ isLoading }) => {
+const Spinner: React.FC<SpinnerProps> = ({ isLoading = true }) => {
   return (
     <div className="flex justify-center items-center h-screen">
       <ClipLoader
