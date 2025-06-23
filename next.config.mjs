39e52c07add41ce6060223c31368ca15b1fdffd3
tanakit <tum_tanakid@hotmail.com>
2025-06-23 20:49:46 +0700

    Fix CSS loading with basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  basePath: '/resume',
+  assetPrefix: '/resume',
+};
 
 export default nextConfig;
